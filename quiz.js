93e19442da80d1c2cff4a383c8b68018ba7e3573
Alex Fore <forealexander4@gmail.com>
2025-09-21 14:38:06 -0500

    created a 5 question quiz that returns users input

--- quiz.js
+++ quiz.js
@@ -1,1 +1,16 @@
+const readline = require('readline-sync');
 
+let question1 = readline.question("What type of value is created when a user inputs a number using the question() method? ");
+console.log (question1, '\n');
+
+let question2 = readline.question("Is the Boolean() function considered an implicit or explicit data conversion? ");
+console.log (question2, '\n');
+
+let question3 = readline.questionInt("What would print in the following code: console.log(Number(345m)); ? ");
+console.log (question3, '\n');
+
+let question4 = readline.question("What do you call a collection of key-value pairs that can represent more complex, structured data? ");
+console.log (question4, '\n');
+
+let question5 = readline.question("Which type of variable cannot be changed once created? ");
+console.log (question5);
